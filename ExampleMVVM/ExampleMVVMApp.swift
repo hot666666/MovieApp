1dac637d4662c3a37e78233be3f8a7d78c442c28
hot666666 <hotcs6@naver.com>
2024-05-08 15:50:14 +0900

    Initial Commit

---
+++ ExampleMVVM/ExampleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleMVVMApp.swift
+//  ExampleMVVM
+//
+//  Created by 최하식 on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
